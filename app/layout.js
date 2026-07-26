115fbe0c8f6bbe1ebf4ffccc5e16f7a4c215d5db
Kira1717171717 <moh.alkhataba@gmail.com>
2026-07-26 16:15:21 +0300

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,39 @@
+import { Cinzel, Noto_Naskh_Arabic } from "next/font/google";
 import "./globals.css";
+
+
+const cinzel = Cinzel({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+});
+
+
+const arabicFont = Noto_Naskh_Arabic({
+  subsets: ["arabic"],
+  weight: ["400", "500", "700"],
+});
+
 
 export const metadata = {
   title: "Mohammad & Manar Wedding",
   description: "Wedding Invitation",
 };
 
+
 export default function RootLayout({ children }) {
+
   return (
-    <html lang="ar" dir="rtl">
-      <body>{children}</body>
+
+    <html lang="ar">
+
+      <body className={`${cinzel.className} ${arabicFont.className}`}>
+
+        {children}
+
+      </body>
+
     </html>
+
   );
+
 }
