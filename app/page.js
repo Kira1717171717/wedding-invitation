dd6cff714df37e8905256bd5a3ef8623ca99e1e6
Kira1717171717 <moh.alkhataba@gmail.com>
2026-07-27 12:44:36 +0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -3,18 +3,30 @@
 import { useState } from "react";
 
 export default function Home() {
+  const [opening, setOpening] = useState(false);
   const [opened, setOpened] = useState(false);
 
+  const handleOpen = () => {
+    setOpening(true);
+
+    setTimeout(() => {
+      setOpened(true);
+    }, 700);
+  };
+
   return (
-    <main
-      className="min-h-screen bg-[#07182f] flex items-center justify-center overflow-hidden"
-      onClick={() => setOpened(true)}
-    >
+    <main className="min-h-screen bg-[#07182f] flex items-center justify-center overflow-hidden">
+
       {!opened ? (
         <img
           src="/envelope.jpg"
           alt="Wedding Envelope"
-          className="w-full h-screen object-contain cursor-pointer transition-transform duration-700 hover:scale-105"
+          onClick={handleOpen}
+          className={`w-full h-screen object-contain cursor-pointer transition-all duration-700 ${
+            opening
+              ? "scale-110 opacity-0"
+              : "scale-100 opacity-100"
+          }`}
         />
       ) : (
         <img
@@ -23,6 +35,7 @@ export default function Home() {
           className="w-full h-screen object-contain animate-in fade-in duration-1000"
         />
       )}
+
     </main>
   );
 }
