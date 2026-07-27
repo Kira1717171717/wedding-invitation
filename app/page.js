fc6eaddc38f7a3a2817cc3a9573a5cf9b575035a
Kira1717171717 <moh.alkhataba@gmail.com>
2026-07-27 13:53:42 +0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -4,6 +4,7 @@ import { useState } from "react";
 
 export default function Home() {
   const [opened, setOpened] = useState(false);
+  const [invitationOpen, setInvitationOpen] = useState(false);
 
   return (
     <main className="min-h-screen bg-[#07182f] flex items-center justify-center overflow-hidden">
@@ -14,42 +15,68 @@ export default function Home() {
           alt="Wedding Envelope"
           onClick={() => setOpened(true)}
           className="max-w-full max-h-screen object-contain cursor-pointer"
-          style={{
-            animation: "fadeIn 0.8s ease",
-          }}
         />
       ) : (
-        <img
-          src="/wedding-bg.jpg"
-          alt="Wedding Invitation"
-          className="max-w-full max-h-screen object-contain"
-          style={{
-            animation: "showCard 0.8s ease",
-          }}
-        />
+        <div className="relative w-full h-screen flex items-center justify-center">
+
+          <img
+            src="/wedding-bg.jpg"
+            alt="Wedding Invitation"
+            className="max-w-full max-h-screen object-contain"
+          />
+
+          {!invitationOpen && (
+            <button
+              onClick={() => setInvitationOpen(true)}
+              className="
+              absolute
+              bottom-20
+              px-10
+              py-4
+              rounded-full
+              bg-[#c9a45c]
+              text-[#07182f]
+              font-semibold
+              tracking-widest
+              shadow-lg
+              "
+            >
+              Open Invitation
+            </button>
+          )}
+
+          {invitationOpen && (
+            <div
+              className="
+              absolute
+              inset-0
+              flex
+              flex-col
+              items-center
+              justify-center
+              text-center
+              "
+            >
+              <h1 className="text-4xl text-[#c9a45c] font-serif">
+                Mohammed & Manar
+              </h1>
+
+              <p className="mt-6 text-white text-xl">
+                05 August 2026
+              </p>
+
+              <p className="mt-4 text-white text-lg">
+                Yildiz Hall
+              </p>
+
+              <p className="text-white">
+                Black Tie - Weddings
+              </p>
+            </div>
+          )}
+
+        </div>
       )}
-
-      <style jsx>{`
-        @keyframes fadeIn {
-          from {
-            opacity: 0;
-          }
-          to {
-            opacity: 1;
-          }
-        }
-
-        @keyframes showCard {
-          from {
-            opacity: 0;
-            transform: scale(0.98);
-          }
-          to {
-            opacity: 1;
-            transform: scale(1);
-          }
-        }
-      `}</style>
 
     </main>
   );
